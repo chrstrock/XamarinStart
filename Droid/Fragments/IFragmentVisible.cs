3798e0d8f2592ed451add055222a563c3f24999e
chrstrock <chrstrock@gmail.com>
2018-05-28 12:59:10 -0400

    First mobile development project using Xamarin.

---
+++ Droid/Fragments/IFragmentVisible.cs
@@ -0,0 +1,7 @@
+﻿namespace FirstProject.Droid
+{
+    interface IFragmentVisible
+    {
+        void BecameVisible();
+    }
+}
---
+++ FirstUnitTest/Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+namespace FirstUnitTest
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+            //arrange
+
+            //act
+
+            //assert
+
+        }
+    }
+}
